0612548e2c6c7e62fedfcb2c364fecabd34b8614
fan <a1756535524@gmail.com>
2019-05-16 21:46:33 +0800

    +62

---
+++ src/Math61.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * @ClassName Math61
+ * @Description leetcode 204
+ * @Author GZfan
+ * @Date 19-5-16
+ */
+public class Math61 {
+
+    public int countPrimes(int n) {
+        boolean[] prime=new boolean[n+1];
+        if (n == 0 || n == 1 || n == 2) {
+            return 0;
+        }
+        int count=1;
+        Arrays.fill(prime,true);
+        for (int i = 4; i < n ; i=i+2) {
+            prime[i]=false;
+        }
+        for (int i = 3; i <n ; i=i+2) {
+            if(prime[i]){
+                for(int j=2,num=j*i;num<n;j++,num=j*i){
+                    prime[num]=false;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+
+}
---
+++ src/Math62.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName Math62
+ * @Description
+ * @Author GZfan
+ * @Date 19-5-16
+ */
+public class Math62 {
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467%n == 0;
+    }
+}
