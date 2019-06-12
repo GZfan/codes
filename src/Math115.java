47f2744561fbcce3612b6d000631047510e6abb2
fan <a1756535524@gmail.com>
2019-06-12 13:07:01 +0800

    +116

---
+++ src/Math115.java
@@ -0,0 +1,26 @@
+/**
+ * @ClassName Math115
+ * @Description
+ * @Author GZfan
+ * @Date 19-6-12
+ */
+public class Math115 {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double currentProduct = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * currentProduct;
+            }
+            currentProduct = currentProduct * currentProduct;
+        }
+        return ans;
+    }
+
+
+}
---
+++ src/Math116.java
@@ -0,0 +1,25 @@
+/**
+ * @ClassName Math116
+ * @Description leetcode 69
+ * @Author GZfan
+ * @Date 19-6-12
+ */
+public class Math116 {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        long right = x, left = 0, mid = left + (right - left) / 2;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (mid * mid > x) {
+                right = mid - 1;
+            } else if ((mid + 1) * (mid + 1) > x) {
+                break;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return new Long(mid).intValue();
+    }
+}
